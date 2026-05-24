5224a714d006f29c6782e41fba3f2f81bc4d27e9
ddrayko <189259481+ddrayko@users.noreply.github.com>
2026-05-24 16:03:52 +0200

    Delete supabase-migration.sql

--- supabase-migration.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "versions" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"description" text,
-	"link" text NOT NULL,
-	"created_at" timestamp DEFAULT now()
-);
