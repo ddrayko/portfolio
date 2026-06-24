260cbe59d4869b1a828dc7d58f739bcc95a10f3f
ddrayko <ddrayko@proton.me>
2026-06-24 17:02:25 +0200

    refactor: typer le schema sans cast any, export direct

--- db/schema.ts
+++ db/schema.ts
@@ -2,16 +2,7 @@ import { dbType } from './config'
 import * as schemaPg from './schema-pg'
 import * as schemaSqlite from './schema-sqlite'
 
-const schema = dbType === 'sqlite' ? schemaSqlite : schemaPg
-
-type SchemaTables = {
-  projects: typeof schemaPg.projects
-  admins: typeof schemaPg.admins
-  settings: typeof schemaPg.settings
-  siteUpdates: typeof schemaPg.siteUpdates
-  moments: typeof schemaPg.moments
-  versions: typeof schemaPg.versions
-}
+const selectedSchema = dbType === 'sqlite' ? schemaSqlite : schemaPg
 
 export const {
   projects,
@@ -20,7 +11,7 @@ export const {
   siteUpdates,
   moments,
   versions,
-}: SchemaTables = schema as any
+} = selectedSchema
 
 export type { DbType } from './config'
 export { dbType } from './config'
